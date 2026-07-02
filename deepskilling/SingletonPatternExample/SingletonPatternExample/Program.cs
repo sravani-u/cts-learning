d90b1d680874b832612e597755e54706fd8cd7b5
sravani-u <sravaniudatha2005@gmail.com>
2026-07-02 09:54:00 +0530

    Completed Exercise 1 - Singleton Pattern

---
+++ deepskilling/SingletonPatternExample/SingletonPatternExample/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonPatternExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("First message");
+            logger2.Log("Second message");
+
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("Only one Logger instance exists.");
+            }
+        }
+    }
+}
