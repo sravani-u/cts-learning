b8de11d1ced688514847ae30d10eeb4d96386288
sravani-u <sravaniudatha2005@gmail.com>
2026-07-03 18:15:11 +0530

    Completed Exercise 7 - Financial Forecasting

---
+++ deepskilling/FinancialForecasting/FinancialForecasting/ForecastCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace FinancialForecasting
+{
+    public class ForecastCalculator
+    {
+        public static double PredictFutureValue(
+            double currentValue,
+            double growthRate,
+            int years)
+        {
+            if (years == 0)
+                return currentValue;
+
+            return PredictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+        }
+    }
+}
---
+++ deepskilling/FinancialForecasting/FinancialForecasting/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FinancialForecasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentValue = 10000;
+
+            double growthRate = 0.10;
+
+            int years = 5;
+
+            double futureValue =
+                ForecastCalculator
+                    .PredictFutureValue(
+                        currentValue,
+                        growthRate,
+                        years);
+
+            Console.WriteLine(
+                $"Future Value after {years} years: {futureValue}");
+        }
+    }
+}
