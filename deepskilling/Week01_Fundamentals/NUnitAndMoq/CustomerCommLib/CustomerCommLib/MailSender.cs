235c229a628107e45cba311ac9cc33d684bde906
sravani-u <sravaniudatha2005@gmail.com>
2026-07-17 21:16:40 +0530

    Started Moq Exercise 1 - Customer Communication Library

---
+++ deepskilling/Week01_Fundamentals/NUnitAndMoq/CustomerCommLib/CustomerCommLib/MailSender.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerCommLib
+{
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            // Simulate sending an email
+            return true;
+        }
+    }
+}
